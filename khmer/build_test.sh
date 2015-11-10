aaef362b5433fdf2707e8a3b3941950008587448
C. Titus Brown <titus@idyll.org>
2015-11-10 18:43:19 +0000

    update & final

--- khmer/build_test.sh
+++ khmer/build_test.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 docker build -t khmer:2.0 .
-docker run -it khmer:2.0 normalize-by-median.py
+
+docker run --rm -v ${PWD}:/output:rw \
+    -it khmer:2.0 \
+    normalize-by-median.py /home/khmer/data/stamps-reads.fa.gz --gzip -o /output/out.fa.gz
