e76597c4519093265baa6b074df7e212b5c38a4d
C. Titus Brown <titus@idyll.org>
2015-11-10 20:26:01 +0000

    updated salmon

--- dammit/create-data-volume.sh
+++ dammit/create-data-volume.sh
@@ -1,10 +1,12 @@
 ### this creates a docker-land volume that can be shared among running
 ### docker containers.
 
+# this is useful for docker-machine
 docker create -v /dammit-db --name dammit-db ubuntu:15.10 /bin/true
 
 docker run --volumes-from dammit-db -it ubuntu:15.10 bash
 
 ### alternatively, you could do:
 
+# this is useful for locally running docker
 docker run -v /some/local/directory:/dammit-db ....
